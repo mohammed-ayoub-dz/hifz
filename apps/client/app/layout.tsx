33457697a575aff4af6ab5810d752644c8606136
mohammed-ayoub-dz <mohammedayoub852@proton.me>
2026-08-29 16:22:45 +0100

    feat: integrate next-themes for improved theme management and add ThemeProvider component

--- apps/client/app/layout.tsx
+++ apps/client/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Providers } from "@/auth/providers";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,7 +27,15 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
     >
       <body className="min-h-full flex flex-col">
          <Providers>
+           <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            
            {children}
+          </ThemeProvider>
          </Providers>
         </body>
     </html>
