6e9d579c6f6f4ba49c342b0cc36e8650e2395d0f
mohammed-ayoub-dz <mohammedayoub852@proton.me>
2026-08-29 18:46:05 +0100

    feat(routes): pass gorm.DB instance to GetMe and CompleteOnboarding handlers

--- apps/api/routes/me/route.go
+++ apps/api/routes/me/route.go
@@ -2,6 +2,7 @@ package me
 
 import (
 	"github.com/gofiber/fiber/v3"
+	"gorm.io/gorm"
 
 	"github.com/mohammed-ayoub-dz/hifz/handlers"
 	"github.com/mohammed-ayoub-dz/hifz/middleware"
@@ -13,5 +14,5 @@ func RegisterRoutes(api fiber.Router) {
 		middleware.Protected(),
 	)
 
-	me.Get("/", handlers.GetMe)
+	me.Get("/", handlers.GetMe(&gorm.DB{}))
 }
