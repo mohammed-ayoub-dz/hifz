b944e466ab3308ee20c25553fefb02612661dcc8
mohammed-ayoub-dz <mohammedayoub852@proton.me>
2026-08-29 03:54:34 +0100

    Add dotenv support and improve database connection handling

--- apps/api/server/main.go
+++ apps/api/server/main.go
@@ -1,12 +1,46 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/gofiber/fiber/v3"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
+var DB *gorm.DB
+
 func main(){
+
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Println("The .env file was not found in the runtime environment.")
+	}
+
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+	sslmode := os.Getenv("DB_SSLMODE")
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
+		host, user, password, dbname, port, sslmode)
+
+	var dbErr error
+
+	DB, dbErr = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if dbErr != nil {
+		log.Fatal("Failed to connect to the database ", dbErr)
+	}
+
+	fmt.Println("Successfully connected to the database.")
+		
 	app := fiber.New();
 
 	log.Fatal(app.Listen(":3000"))
